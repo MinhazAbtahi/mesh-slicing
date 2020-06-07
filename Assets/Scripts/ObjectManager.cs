5fe4987eceb75250e58b59167e7be2a05c46d8d0
MinhazAbtahi <abtahi.7cgen@gmail.com>
2020-06-07 13:50:22 +0600

    Sof Body Slicing Working

---
+++ Assets/Scripts/ObjectManager.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using BzKovSoft;
+using UnityEngine.SceneManagement;
+using JellyCube;
+
+public class ObjectManager : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> sliceObjects;
+    [SerializeField]
+    private GameObject sliceObject;
+    [SerializeField]
+    private float targetPositionX;
+    [SerializeField]
+    private float step;
+    private bool isMoving;
+    private int totalMove = 0;
+    public bool isGameOver;
+    public bool isGameStart;
+    public int levelsCount = 0;
+
+    public GameObject ui;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        levelsCount = PlayerPrefs.GetInt("LevelsCount", 0);
+        sliceObject = sliceObjects[levelsCount];
+        sliceObject.transform.position = new Vector3(0f, sliceObject.transform.position.y, 0f);
+        sliceObject.SetActive(true);
+
+        sliceObject.transform.DOMoveX(targetPositionX, 1f).OnComplete(()=>
+        {
+            sliceObject.GetComponent<RubberEffect>().enabled = true;
+            isGameStart = true;
+        });
+        step = sliceObject.transform.localScale.x / 3f;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void MoveForward()
+    {
+        if (totalMove <= 2)
+        {
+            targetPositionX += step;
+            //targetPositionX += sliceObject.transform.localScale.x;
+            StartCoroutine(MoveRoutine());
+        }
+    }
+
+    public void StopMoving()
+    {
+        StopAllCoroutines();
+    }
+
+    private IEnumerator MoveRoutine()
+    {
+        yield return new WaitForSeconds(.1f);
+        isMoving = true;
+        sliceObject.transform.DOMoveX(targetPositionX, 1f).OnComplete(()=>
+        {
+            ui.SetActive(isGameOver);
+        });
+
+        ++totalMove;
+
+        if (totalMove >= 3)
+        {
+            isGameStart = false;
+            isGameOver = true;
+            ++levelsCount;
+            if (levelsCount >= sliceObjects.Count)
+            {
+                levelsCount = levelsCount % (sliceObjects.Count);
+            }
+            PlayerPrefs.SetInt("LevelsCount", levelsCount);
+        }
+    }
+
+    public void Reload()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,105 @@
+﻿using UnityEngine;
+using BzKovSoft.ObjectSlicer;
+using System;
+using System.Collections;
+using BzKovSoft.ObjectSlicerSamples;
+using DG.Tweening;
+
+public class PlayerController : MonoBehaviour
+{
+    public ObjectManager objectManager;
+
+    [Header("Blade Prefab:")]
+    [SerializeField]
+    private GameObject _blade;
+    private Transform bladeTransform;
+    private BzKnife knife;
+
+    [Space(5)]
+    [Header("Cotroller Values:")]
+    [SerializeField]
+    private float maxY = 10.5f;
+    [SerializeField]
+    private float minY = 7.5f;
+    [SerializeField]
+    private float duration = .5f;
+    private bool gone;
+
+    private void Start()
+    {
+        bladeTransform = _blade.transform;
+        knife = _blade.GetComponentInChildren<BzKnife>();
+    }
+
+    void Update()
+    {
+        if (objectManager.isGameOver)
+        {
+            if (!gone)
+            {
+                gone = true;
+                bladeTransform.DOMoveY(maxY * 2, duration * 2);
+            }
+            return;
+        }
+
+        if (!objectManager.isGameStart)
+        {
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            //objectManager.MoveForward();
+        }
+
+        if (Input.GetMouseButton(0))
+        {
+            //knife.BeginNewSlice();
+
+            KnifeDown();
+        }
+
+        if (Input.GetMouseButtonUp(0))
+        {
+            KnifeUp();
+        }
+    }
+
+    IEnumerator SwingSword()
+    {
+        var transformB = _blade.transform;
+        //transformB.position = Camera.main.transform.position;
+        //transformB.rotation = Camera.main.transform.rotation;
+
+        const float seconds = .5f;
+        for (float f = 0f; f < seconds; f += Time.deltaTime)
+        {
+            float aY = (f / seconds) * 180 - 90;
+            //float aX = (f / seconds) * 60 - 30;
+            float aX = (f / seconds) * 90;
+
+            //var r = Quaternion.Euler(aX, -aY, 0);
+            var r = Quaternion.Euler(-aX, 0, 0);
+
+            transformB.rotation = /*Camera.main.transform.rotation **/ r;
+            yield return null;
+        }
+    }
+
+    private void KnifeDown()
+    {
+        objectManager.StopMoving();
+        bladeTransform.DOKill();
+        bladeTransform.DOMoveY(minY, duration);
+    }
+
+    private void KnifeUp()
+    {
+        bladeTransform.DOKill();
+        bladeTransform.DOMoveY(maxY, duration/2f).OnComplete(()=>
+        {
+            objectManager.MoveForward();
+        });
+    }
+}
