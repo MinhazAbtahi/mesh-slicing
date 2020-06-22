1abf164427b39ddfff7c3fc54708d7472203975b
Sakib (unity) <iphone@itiw-webdev.com>
2020-06-22 18:57:00 +0600

    final commit for thickness curve

--- Assets/Frameworks/BzKovSoft/ObjectSlicer/EventHandlers/BzFixMassSmart.cs
+++ Assets/Frameworks/BzKovSoft/ObjectSlicer/EventHandlers/BzFixMassSmart.cs
@@ -27,10 +27,12 @@ namespace BzKovSoft.ObjectSlicer.EventHandlers
 
 			Rigidbody rigidO = original.GetComponent<Rigidbody>();
 
-			Rigidbody rigidA = resultPos.GetComponent<Rigidbody>();
-			Rigidbody rigidB = resultNeg.GetComponent<Rigidbody>();
+            //Rigidbody rigidA = resultNeg.GetComponent<Rigidbody>();
+            //Rigidbody rigidB = resultPos.GetComponent<Rigidbody>();
+            Rigidbody rigidA = resultPos.GetComponent<Rigidbody>();
+            Rigidbody rigidB = resultNeg.GetComponent<Rigidbody>();
 
-		    float massO = rigidO.mass;
+            float massO = rigidO.mass;
             Vector3 centerOfMassA;
 			Vector3 centerOfMassB;
 
